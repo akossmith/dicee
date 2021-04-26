7ac8d136cf54dab909e370c82bf050dc6e2d05dc
Akos Kovacs <mr.akos.smith@gmail.com>
2021-04-26 19:54:33 +0200

    dicee name editing implemented

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.querySelector("a#click-to-roll").addEventListener("click",(e) => {
     e.preventDefault();
     roll();
     return false;
-})
+});
+
+// let players = ["Player 1", "Player 2"];
 
 function roll() {
     let rand = () => Math.floor(Math.random() * 6) + 1;
@@ -44,5 +46,43 @@ function displayWinner(winnerIndices){
 function putFlags(winnerIndices){
     let flagDivs = document.querySelectorAll(".flag");
     flagDivs.forEach(div => div.innerHTML = "");
-    winnerIndices.forEach(ind => flagDivs[ind].innerHTML = "🚩")
+    winnerIndices.forEach(ind => flagDivs[ind].innerHTML = "🚩");
 }
+
+
+// name editing
+
+document.querySelectorAll(".player-name").forEach(elem => elem.addEventListener("click", (event) => {
+    let form = elem.parentNode.querySelector("form");
+    let textInput = form.querySelector("input[type='text'");
+    textInput.value = elem.textContent;
+    toggleEdit(elem.parentElement);
+    textInput.focus();
+}));
+
+let forms = document.querySelectorAll("form");
+forms.forEach(form => form.addEventListener("submit", () => {
+    let nameElem = form.closest(".player").querySelector(".player-name");
+    nameElem.innerHTML = form.firstElementChild.value;
+    toggleEdit(form.parentElement);
+}));
+
+document.querySelectorAll("input[type='text']").forEach(textIntput => textIntput.addEventListener("blur", () => {
+    if (! textIntput.parentElement.classList.contains("hidden"))
+        toggleEdit(textIntput.parentElement.parentElement);
+    //event.target ?????????
+}));
+
+document.querySelectorAll("input[type='text']").forEach(textIntput => textIntput.addEventListener("keydown", (event) => {
+    var x = event.keyCode;
+    if (x == 27) {
+        toggleEdit(textIntput.parentElement.parentElement);
+    }
+}));
+
+function toggleEdit(playerH2){
+    playerH2.children[0].classList.toggle("hidden");
+    playerH2.children[1].classList.toggle("hidden");
+}
+
+// adding new player
